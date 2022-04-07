21670fbccf175c1e241a2923f5ea76499b21dae7
JJovanovic <jjovanovic@noteris.com>
2022-04-07 15:23:20 +0300

    Creating Message and loader components and applying them in the homescreen page

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
   const dispatch = useDispatch()
 
-  const productList = useSelector(state => state.productList)
+  const productList = useSelector((state) => state.productList)
   const { loading, error, products } = productList
 
   useEffect(() => {
@@ -18,12 +20,12 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
-          {products.map(product => (
+          {products.map((product) => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
               <Product product={product} />
             </Col>
