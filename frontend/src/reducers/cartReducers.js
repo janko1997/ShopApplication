8dadc92a874d657b1108d4510bb0a47ed86a1ef9
JJovanovic <jjovanovic@noteris.com>
2022-04-11 15:18:03 +0300

    fixed typo

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,18 +1,16 @@
 import { CART_ADD_ITEM } from '../constants/cartConstants'
 
-export const cartReducer = (state = { cartItems: [] }, action) => {
+export const cartReducer = (state = { cartItems: [] }, action)  => {
   switch (action.type) {
     case CART_ADD_ITEM:
-      const item = action.paylod
-      debugger
-      console.log(state.cartItems)
+      const item = action.payload
+
       const existItem = state.cartItems.find((x) => x.product === item.product)
 
       if (existItem) {
-        console.log(state.cartItems)
         return {
           ...state,
-          cartItems: state.cartItems.map((x) => (x.product === existItem.product ? item : x)),
+          cartItems: state.cartItems.map((x) => x.product === existItem.product ? item : x),
         }
       } else {
         return {
