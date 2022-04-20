db199d995e343f062257c8d66ce059acc00d4c8b
JJovanovic <jjovanovic@noteris.com>
2022-04-20 14:19:20 +0300

    edit product screen

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -30,11 +30,11 @@ const ProductListScreen = ({ history, match }) => {
   useEffect(() => {
     dispatch({ type: PRODUCT_CREATE_RESET })
 
-    if (!userInfo.isAdmin) {
+    if (!userInfo || !userInfo.isAdmin) {
       history.push('/login')
     }
     if (successCreate) {
-      history.push(`/admin/product/${createProduct._id}/edit`)
+      history.push(`/admin/product/${createdProduct._id}/edit`)
     } else {
       dispatch(listProducts())
     }
